fix accuracy calculation

src/training_operations.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from transformers import AdamW
 from tqdm import tqdm
+from sklearn.metrics import accuracy_score
 
 
 def train_function(data, model, opt_param, lr):
@@ -27,8 +28,8 @@ def train_function(data, model, opt_param, lr):
 
 def eval_function(data, model):
     model.eval()
-    correct = 0
-    total = 0
+    true = []
+    pred = []
     with torch.no_grad():
         for idx, value in tqdm(enumerate(data), total=len(data)):
 
@@ -38,13 +39,13 @@ def eval_function(data, model):
             targets = value[3].view(-1, 1)
 
             outputs = model(input_ids=ids, attention_mask=mask, token_type_ids=token_ids)
-           
-            total += targets.size(0)
             predicts = torch.gt(outputs, 0.5).double()
-            correct += (predicts == targets).sum().item()
 
-    acc = (correct/total)*100        
-    print(f'Accuracy {acc}%')
+            true.extend(targets.squeeze().tolist())
+            pred.extend(predicts.squeeze().tolist())
+
+    acc = accuracy_score(y_true=true, y_pred=pred)
+    print(f"Accuracy : {acc}")
 
 
 def loss_func(pred, true):